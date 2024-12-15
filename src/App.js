dd73827a2e36f73e9f149cdefb8264f840cae040
misawa-shota <124896325+misawa-shota@users.noreply.github.com>
2024-12-15 17:07:19 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { CounterA } from "./CounterA";
+import { CounterB } from "./CounterB";
+import { CounterC } from "./CounterC";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <CounterA />
+      <CounterB />
+      <CounterC />
+    </>
+  );
+}
---
+++ src/CounterText.js
@@ -0,0 +1,1 @@
+export const CounterText = ({ count }) => <p>現在のカウント数:{count}</p>;
